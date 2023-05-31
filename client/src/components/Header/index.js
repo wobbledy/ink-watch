ed47ab31aad913cd9dab1e98c73b5eded6d7b47a
jmcdlungren <j.mcd.lungren@gmail.com>
2023-05-30 21:22:11 -0700

    updated header text in header index.js

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -12,12 +12,12 @@ const Header = () => {
   return (
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <img src={Images} alt='Logo' className='Images'></img>
+        <img src={Images} alt='Logo' width='250px' className='Images'></img>
         <div>
           <Link className="text-light" to="/">
-            <h1 className="m-0">Tech Thoughts</h1>
+            <h1 className="m-0">Ink Watch</h1>
           </Link>
-          <p className="m-0">Get into the mind of a programmer.</p>
+          <p className="m-0">Find Your Ink</p>
         </div>
         <div>
           {Auth.loggedIn() ? (
