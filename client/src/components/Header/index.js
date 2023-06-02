ce1f88100cec90fef503485836e470c45b49cacb
Blake Reece <frankiereece3@gmail.com>
2023-06-01 21:50:14 -0700

    added background image on header. changed margin etc.

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Images from '../../images/logo.png'
+//import Images from '../../images/logo.png'
+
 
 import Auth from '../../utils/auth';
 
@@ -10,9 +11,10 @@ const Header = () => {
     Auth.logout();
   };
   return (
+    <header className="header">
     <header className="bg-primary text-light mb-4 py-3 flex-row align-center">
       <div className="container flex-row justify-space-between-lg justify-center align-center">
-        <img src={Images} alt='Logo' width='250px' className='Images'></img>
+        {/* <img src={Images} alt='Logo' width='250px' className='Images'></img> */}
         <div>
           <Link className="text-light" to="/">
             <h1 className="m-0">Ink Watch</h1>
@@ -41,6 +43,7 @@ const Header = () => {
           )}
         </div>
       </div>
+      </header>
     </header>
   );
 };
