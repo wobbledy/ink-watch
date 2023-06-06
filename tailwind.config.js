9b01793b1581819cb1ae87e0ec51b9cc11d079d5
Blake Reece <frankiereece3@gmail.com>
2023-06-05 19:02:58 -0700

    setting up intial tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./client/src/components/**/*.{js,jsx,ts,tsx}', './client/src/pages/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
