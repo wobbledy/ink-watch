8e4317959ef977b4d1580db023d976fe081b70ab
Blake Reece <frankiereece3@gmail.com>
2023-06-08 19:58:28 -0700

    added shadowing to give the box dimension

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -18,14 +18,18 @@ const Header = () => {
           <Link className="text-light" to="/">
             <h1 className="m-0">Ink Watch</h1>
           </Link>
-          <p className="m-0">Find Your Ink</p>
+          {/* <p className="m-0">Find Your Ink</p> */}
         </div>
         <div>
           {Auth.loggedIn() ? (
             <>
-              <Link className="btn btn-lg btn-info m-2" to="/me">
+                <Link className="btn btn-lg btn-info m-2" to="/me" style={{ position: 'absolute', left: '0' }}>
+                  {Auth.getProfile().data.username}'s profile
+                </Link>
+
+              {/* <Link className="btn btn-lg btn-info m-2" to="/me">
                 {Auth.getProfile().data.username}'s profile
-              </Link>
+              </Link> */}
               <button className="btn btn-lg btn-light m-2" onClick={logout}>
                 Logout
               </button>
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -53,7 +53,14 @@ const Profile = () => {
         {!userParam && (
           <div
             className="col-12 col-md-10 mb-3 p-3"
-            style={{ borderRadius: '6px', borderColor: 'blanchedalmond', borderStyle: 'double', borderWidth: 'thick', }}
+            style={{
+              borderRadius: '6px',
+              borderColor: 'blanchedalmond',
+              borderStyle: 'inset',
+              borderWidth: 'thick',
+              boxShadow: 'rgba(22, 31, 39, 0.42) 0px 60px 123px -25px, rgba(19, 26, 32, 0.08) 0px 35px 75px -35px'
+            }}
+            
           >
             <PostForm />
           </div>
