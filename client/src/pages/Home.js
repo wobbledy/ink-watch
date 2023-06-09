94e12c137f6df6749651c42ac328dcb5490ebfe3
Blake Reece <frankiereece3@gmail.com>
2023-06-08 20:38:31 -0700

    added style for post and comment

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -20,12 +20,12 @@ const Home = () => {
             borderColor: 'blanchedalmond',
             borderStyle: 'inset',
             borderWidth: 'thick',
-            boxShadow: 'rgba(22, 31, 39, 0.42) 0px 60px 123px -25px rgba(19, 26, 32, 0.08) 0px 35px 75px -35px'
+            boxShadow: 'rgba(22, 31, 39, 0.42) 0px 60px 123px -25px, rgba(19, 26, 32, 0.08) 0px 35px 75px -35px'
           }}
         >
           <PostForm />
         </div>
-        <div className="col-12 col-md-8 mb-3">
+        <div className="col-12 col-md-8 mb-3" style={{ backgroundColor: 'white', border: '1px solid #ddd', borderRadius: '5px', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', padding: '20px', color: 'black', fontSize: '1rem' }}>
           {loading ? (
             <div>Loading...</div>
           ) : (
