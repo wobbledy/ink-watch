95730e28e41dc2c9671e6f3a9f36d663d9196d33
Alexis <alexis_keeter@yahoo.com>
2023-06-08 19:38:46 -0700

    fixed merging error

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -64,7 +64,7 @@ const typeDefs = gql`
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
-    loginUser(username: String, password: String!): Auth
+    loginUser(username: String, email: String, password: String!): Auth
     addPost(postText: String!, postAuthor: String!): Post
     addComment(postId: ID!, commentText: String!): Post
     removePost(postId: ID!): Post
